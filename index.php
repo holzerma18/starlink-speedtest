2525746ef804396140c66c293b3172bf16d942b0
holzerma18 <56685681+holzerma18@users.noreply.github.com>
2022-12-10 11:49:42 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<settings>
+<servers>
+  <server url="http://speedtest.citycom-austria.com:8080/speedtest/upload.php" lat="47.0703" lon="15.4389" name="Graz" country="Austria" cc="AT" sponsor="Citycom Telekommunikation GmbH" id="3645" host="speedtest.citycom-austria.com:8080"/>
+</servers>
+</settings>
